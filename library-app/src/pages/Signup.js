5b48a0f1a9b64f07fe54912f27c3e6509e771bb0
Muhammed Sinan P <73425914+Sinanp1x@users.noreply.github.com>
2025-09-19 02:35:10 +0000

    packchange

--- library-app/src/pages/Signup.js
+++ library-app/src/pages/Signup.js
@@ -16,7 +16,7 @@ function Signup() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/signup', {
+      const response = await fetch('/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
